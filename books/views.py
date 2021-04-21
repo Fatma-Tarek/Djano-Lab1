9822300b523f5fc0d62c8d86441792df2fb35037
Fatma <fatma.tarek204@gmail.com>
2021-04-21 17:23:03 +0200

    List all books

--- books/views.py
+++ books/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import BookForm
+from .models import Book
 
 # Create your views here.
 #request mandatory 
 def index(request):
+    books = Book.objects.all() #fetch all books
     #same as html but I don't write html in view 
     #return HttpResponse("Hello from books")
-    return render(request,"books/index.html")
+    return render(request,"books/index.html",{
+        "books" : books
+    })
 
 def create(request):
     form = BookForm(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect("index")
+
     return render(request,"books/create.html",{
         "form" : form
     })
