5d33e1bdf153259ffa616daf70f91a2884350695
Fatma <fatma.tarek204@gmail.com>
2021-04-21 17:10:27 +0200

    connect to database and add method create book

---
+++ books/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views # mean speak any method of views
+
+# name important bs . if I change path 
+urlpatterns = [
+    path("",views.index,name="index"),
+    path("create",views.create,name="create")
+]
