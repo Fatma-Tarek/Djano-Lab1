91e6cff0d74f2f986da4b771125f230d014cd1ab
Fatma <fatma.tarek204@gmail.com>
2021-04-25 00:59:01 +0200

    convert name

--- books/api/views.py
+++ books/api/views.py
@@ -4,6 +4,7 @@ from books.models import Book
 from .serializers import BookSerializer , UserSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.parsers import JSONParser 
 from django.http.response import JsonResponse
 
 
@@ -73,10 +74,10 @@ def create(request):
 @api_view(["PUT"])
 def update(request,id):
     book = Book.objects.get(pk=id)
-    tutorial_serializer = BookSerializer(data=request.data, instance=book, partial=True)
-    if tutorial_serializer.is_valid(): 
-        tutorial_serializer.save() 
-        return JsonResponse(tutorial_serializer.data) 
-    return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+    book_serializer = BookSerializer(book , data=request.data) 
+    if book_serializer.is_valid(): 
+        book_serializer.save() 
+        return JsonResponse(book_serializer.data) 
+    return JsonResponse(book_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
     
